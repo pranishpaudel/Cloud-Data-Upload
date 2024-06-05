8411bdf80498733fa580379da03f3cab9f37e026
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-06-05 11:24:54 +0545

    fix: :bug:

--- app/api/sendOtp/route.ts
+++ app/api/sendOtp/route.ts
@@ -2,9 +2,11 @@ import { EmailTemplate } from "@/components/email-template";
 import prisma from "@/lib/db/db.config";
 import { generateOTP } from "@/lib/otpGen";
 import { NextResponse, NextRequest } from "next/server";
+import { renderToStaticMarkup } from "react-dom/server";
 
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const reqBody = await req.json();
@@ -41,11 +43,16 @@ export async function POST(req: NextRequest, res: NextResponse) {
         otpExpires: expiryTime,
       },
     });
+
+    const emailHtml = renderToStaticMarkup(
+      EmailTemplate({ firstName: fullName, otp: otp })
+    );
+
     const { data, error } = await resend.emails.send({
       from: "PShow <noreply@pranishpdl.ai>",
       to: ["pranishisop@gmail.com"],
       subject: "Otp verification",
-      react: EmailTemplate({ firstName: fullName, otp: otp }),
+      html: emailHtml,
     });
 
     if (error) {
