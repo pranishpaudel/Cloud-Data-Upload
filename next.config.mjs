8f5076961a1618ab22e4c65a80e2cfbaa8d44d96
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2024-06-05 22:05:31 +0545

    fix: :bug:

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "faces.nyc3.digitaloceanspaces.com",
-        pathname: "**", // Matches all paths under the specified hostname
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.deepai.org",
-        pathname: "**",
-      },
+    domains: [
+      "faces.nyc3.digitaloceanspaces.com",
+      "images.pexels.com",
+      "api.deepai.org",
     ],
   },
   reactStrictMode: false,
